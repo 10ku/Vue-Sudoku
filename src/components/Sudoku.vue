eba2334ddaff207166bb2b9ae15e98f760d38f40
10ku <keprosj@gmail.com>
2020-08-31 22:36:36 -0500

    Valid boards can now be generated

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -6,14 +6,18 @@
 			<td v-for="(value, index) in array" :key=value.id :class="(index === 2 || index === 5) ? 'boldGridVert' : null">{{value ? value : null}}</td>
 		</tr>
 	</table>
-	<button @click="testHor3Button()">testHor3Button</button>
-	<button @click="testVert2Button()">testVert2Button</button>
-	<button @click="testSquare55Button()">testSquare55Button</button>
+	<!-- <button @click="testHor3Button()">testHor3Button</button> -->
+	<!-- <button @click="testVert2Button()">testVert2Button</button> -->
+	<!-- <button @click="testSquare55Button()">testSquare55Button</button> -->
+	<button @click="createSudokuBoard()">createSudokuBoard</button>
+	<button @click="printSudokuBoard()">printSudokuBoard</button>
+	<!-- <button @click="testCheckAllForValue()">testCheckAllForValue</button> -->
+	<!-- <button @click="testGetShuffledArray()">testGetShuffledArray</button> -->
 </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 import GridService from '../services/GridService';
 
 @Component
@@ -30,21 +34,43 @@ export default class Sudoku extends Vue
 						[0,0,2,0,1,0,0,0,0],
 						[0,0,0,0,4,0,0,0,9]]; */
 
+	/* grid: number[][] = [[5,3,4,6,7,8,9,1,2],
+						[6,7,2,1,9,5,3,4,8],
+						[1,9,8,3,4,2,5,6,7],
+						[8,5,9,7,6,1,4,2,3],
+						[4,2,6,8,5,3,7,9,1],
+						[7,1,3,9,2,4,8,5,6],
+						[9,6,1,5,3,7,2,8,4],
+						[2,8,7,4,1,9,6,3,5],
+						[3,4,5,2,8,6,1,7,9]]; */
+
+	/* grid: number[][] = [[5,3,4,6,7,8,9,1,2],
+						[6,7,2,1,9,5,3,4,8],
+						[1,9,8,3,4,2,5,6,7],
+						[8,5,9,7,6,1,4,2,3],
+						[4,2,6,8,5,3,7,9,1],
+						[7,1,3,9,0,4,8,5,6],
+						[9,6,1,5,3,7,2,8,4],
+						[2,8,7,4,1,9,6,3,5],
+						[3,4,5,2,0,0,1,7,0]]; */
+
 	grid: number[][] = [];
 	gridService = new GridService(this.grid)
 	
 
 	created()
 	{
-		for(let i = 0; i < 9; i++)
+		for (let i = 0; i < 9; i++)
 		{
 			this.grid.push([]);
-			for(let j = 0; j < 9; j++)
+			for (let j = 0; j < 9; j++)
 			{
-				// this.grid[i].push(0);
-				this.grid[i].push(Math.floor(Math.random() * 9) + 1);
+				this.grid[i].push(0);
+				// this.grid[i].push(Math.floor(Math.random() * 9) + 1);
 			}
 		}
+
+		// this.gridService
 	}
 
 	private testHor3Button()
@@ -58,6 +84,23 @@ export default class Sudoku extends Vue
 	private testSquare55Button()
 	{
 		console.log(this.gridService.checkSquareOfIndex(8, 6));
+	}
+	private createSudokuBoard()
+	{
+		this.gridService.createSudokuBoard();
+	}
+	private printSudokuBoard()
+	{
+		console.log(this.grid.flat().toString());
+	}
+	/* private testGetShuffledArray()
+	{
+		console.log(this.gridService.getShuffledArray().toString());
+	} */
+	private testCheckAllForValue()
+	{
+		console.log(this.gridService.checkAllForValue(3, 0, 4));
+		// console.log(this.grid.flat().indexOf(0));
 	}
 }
 </script>
--- src/services/GridService.ts
+++ src/services/GridService.ts
@@ -16,25 +16,25 @@ export default class GridService
 	{
 		const array: number[] = [];
 
-		for(let i = 0; i < 9; i++)
+		for (let y = 0; y < 9; y++)
 		{
-			array.push(this.array2d[i][x]);
+			array.push(this.array2d[y][x]);
 		}
 
 		// console.log(array.toString());
 		return this.isArrayUnique(array);
 	}
 
-	checkSquareOfIndex(x: number, y: number)
+	checkSquareOfIndex(x: number, y: number): boolean
 	{
 		const xRoot = x - (x % 3);
 		let xRootIndex = xRoot;
 		let yRoot = y - (y % 3);
 		const array: number[] = [];
 
-		for(let i = 0; i < 3; i++)
+		for (let y = 0; y < 3; y++)
 		{
-			for(let i = 0; i < 3; i++)
+			for (let x = 0; x < 3; x++)
 			{
 				array.push(this.array2d[yRoot][xRootIndex]);
 				xRootIndex++;
@@ -43,13 +43,133 @@ export default class GridService
 			yRoot++;
 		}
 
-		console.log(array.toString());
+		// console.log(array.toString());
 		return this.isArrayUnique(array);
 	}
 
-	isArrayUnique(array: number[]): boolean
+	checkAllForValue(x: number, y: number, n: number): boolean
+	{
+		let isValid = false;
+
+		this.array2d[y].splice(x, 1, n);
+
+		if ((this.checkHorizontalOfIndex(y) === false) || (this.checkVerticalOfIndex(x) === false) || (this.checkSquareOfIndex(x, y) === false))
+		{
+			isValid = false;
+		}
+		else
+		{
+			isValid = true;
+		}
+
+		this.array2d[y].splice(x, 1, 0);
+		return isValid;
+	}
+
+	private isArrayUnique(array: number[]): boolean
 	{
 		const filteredArray = array.filter(value => value !== 0);
 		return new Set(filteredArray).size === filteredArray.length;
+	}
+
+	createSudokuBoard()
+	{
+		this.fillSquaresDiagonally();
+		this.fillRemaining();
+		//this.removeCells(30);
+	}
+
+	private fillSquaresDiagonally()
+	{
+		let xIndex = 0;
+		let xRoot = 0;
+		let yIndex = 0;
+		let done = false;
+
+		while (!done)
+		{
+			const array = this.getShuffledArray();
+			
+			for (let y = 0; y < 3; y++)
+			{
+				xIndex = xRoot;
+				for (let x = 0; x < 3; x++)
+				{
+					this.array2d[yIndex].splice(xIndex, 1, array.pop() as number);
+					xIndex++;
+				}
+				yIndex++;
+			}
+			xRoot += 3;
+
+			if (yIndex === 9)
+			{
+				done = true
+			}
+		}
+	}
+
+	private fillRemaining()
+	{
+		for (let y = 0; y < 9; y++)
+		{
+			for (let x = 0; x < 9; x++)
+			{
+				if (this.array2d[y][x] === 0)
+				{
+					for (let i = 1; i <= 9; i++)
+					{
+						if (this.checkAllForValue(x, y, i))
+						{
+							this.array2d[y].splice(x, 1, i);
+							this.fillRemaining()
+							if(this.array2d.flat().indexOf(0) === -1)
+							{
+								return;
+							}
+						}
+					}
+					this.array2d[y].splice(x, 1, 0);
+					return;
+				}
+			}
+		}
+	}
+
+	private removeCells(n: number)
+	{
+		let x = 0;
+		let y = 0;
+		let removed = 0;
+
+		while (removed != n)
+		{
+			x = Math.floor(Math.random() * 9);
+			y = Math.floor(Math.random() * 9);
+
+			if (this.array2d[y][x] !== 0)
+			{
+				this.array2d[y].splice(x, 1, 0);
+				this.array2d[8-y].splice(8-x, 1, 0);
+				removed += 2;
+			}
+		}
+		// console.log(removed);
+	}
+
+	private getShuffledArray(): number[]
+	{
+		const array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+		//Fisher–Yates shuffle
+		for (let i = array.length - 1; i > 0; i--)
+		{
+			const j = Math.floor(Math.random() * (i + 1))
+			const temp = array[i]
+			array[i] = array[j]
+			array[j] = temp
+		}
+
+		return array;
 	}
 }
